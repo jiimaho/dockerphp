bb6bf30cf372d5048d5907016bad94223725a6b6
Jim Aho <Jim.Aho@collectorbank.se>
2018-11-17 16:15:15 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run --name=my-running-php-app -it --rm -p 8080:80 my-php-app
---
+++ src/index.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello, World!</p>'; ?>
+ <h1>
+ Hello mike!
+ </h1>
+ <h1>
+ Running php inside apache webserver. No need to have php installed on your local computer, nor apache.
+ </h1>
+ <h2>
+ This is all running inside docker, accessible on port 8080 on your local computer!
+ </h2>
+ </body>
+</html>
